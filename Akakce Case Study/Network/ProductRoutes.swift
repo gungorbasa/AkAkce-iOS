2a92951f3afc710bcb2200227e9967c599b0b5dd
Güngör <gungorbasa@gmail.com>
2024-07-31 19:31:59 +0300

    Add network routes

---
+++ Akakce Case Study/Network/ProductRoutes.swift
@@ -0,0 +1,45 @@
+//
+//  ProductRoutes.swift
+//  Akakce Case Study
+//
+//  Created by Gungor Basa on 7/31/24.
+//
+
+import Foundation
+import Networking
+
+enum ProductRoutes: Routing {
+    var host: String { "https://fakestoreapi.com" }
+    
+    var path: String {
+        switch self {
+        case .product:
+            return "/products"
+        case .horizontalProduct:
+            return "/products"
+        case .productDetails(let id):
+            return "/products/\(id)"
+        }
+    }
+    
+    var headers: [String : String] { [:] }
+    
+    var parameters: [String : Any] {
+        switch self {
+        case .product:
+            return [:]
+        case .horizontalProduct:
+            return ["limit": 5]
+        case .productDetails(let id):
+            return [:]
+        }
+    }
+    
+    var body: [String : Any] { [:] }
+    
+    var method: HTTPMethod { .get }
+    
+    case product
+    case horizontalProduct
+    case productDetails(id: String)
+}
