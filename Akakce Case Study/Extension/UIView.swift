a58b0a3025165886197f9c16f2eb9ac3e3f36e5c
Güngör <gungorbasa@gmail.com>
2024-08-01 10:25:12 +0300

    Add ProductListHeaderCell

--- Akakce Case Study/Extension/UIView.swift
+++ Akakce Case Study/Extension/UIView.swift
@@ -5,4 +5,36 @@
 //  Created by Gungor Basa on 8/1/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    /// Initializes autolayout `view` instance
+    class func autolayoutView() -> Self {
+        let instance = self.init()
+        instance.translatesAutoresizingMaskIntoConstraints = false
+        return instance
+    }
+    
+    /// Tranforms existing view into autolayout
+    func autolayoutView() -> Self {
+        translatesAutoresizingMaskIntoConstraints = false
+        return self
+    }
+    
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
+
+extension UIStackView {
+    /// Initializes autolayout `stackView` instance with axis
+    class func autolayoutView(axis: NSLayoutConstraint.Axis) -> Self {
+        let instance = self.init()
+        instance.translatesAutoresizingMaskIntoConstraints = false
+        instance.axis = axis
+        return instance
+    }
+}
---
+++ Akakce Case Study/Scenes/ProductListScene/HorizontalPagerView/ProductListHeaderCell.swift
@@ -0,0 +1,91 @@
+//
+//  ProductListHeaderCell.swift
+//  Akakce Case Study
+//
+//  Created by Gungor Basa on 8/1/24.
+//
+
+import UIKit
+
+final class ProductListHeaderCell: UICollectionViewCell {
+    struct State: Equatable {
+        let image: String
+        let title: String
+        let price: Double
+        
+        var formattedPrice: String { String(format: "%.1f", price) + " TL" }
+    }
+    
+    private let horizontalStackView = UIStackView.autolayoutView(axis: .horizontal)
+    private let titleLabel = UILabel.autolayoutView()
+    private let priceLabel = UILabel.autolayoutView()
+    private let imageView = UIImageView.autolayoutView()
+    private let verticalStackView = UIStackView.autolayoutView(axis: .vertical)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func configure(with state: State) {
+        // TODO: Setup ImageView with caching
+        titleLabel.text = state.title
+        priceLabel.text = state.formattedPrice
+    }
+}
+
+private extension ProductListHeaderCell {
+    func setup() {
+        contentView.addSubview(horizontalStackView)
+        
+        setupHorizontalStackView()
+        setupImageView()
+        setupVerticalStackView()
+        setupTitleLabel()
+        setupPriceLabel()
+        
+        NSLayoutConstraint.activate(
+            [
+                horizontalStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+                horizontalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+                horizontalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                horizontalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+            ]
+        )
+    }
+    
+    func setupHorizontalStackView() {
+        horizontalStackView.addArrangedSubview(imageView)
+        horizontalStackView.addArrangedSubview(verticalStackView)
+    }
+    
+    func setupImageView() {
+        imageView.backgroundColor = .white
+        
+        NSLayoutConstraint.activate(
+            [
+                imageView.heightAnchor.constraint(equalToConstant: 120),
+                imageView.widthAnchor.constraint(equalToConstant: 80)
+            ]
+        )
+    }
+    
+    func setupVerticalStackView() {
+        verticalStackView.addArrangedSubview(titleLabel)
+        verticalStackView.addArrangedSubview(priceLabel)
+    }
+    
+    func setupTitleLabel() {
+        titleLabel.textColor = .gray
+    }
+    
+    func setupPriceLabel() {
+        priceLabel.textColor = .black
+        priceLabel.font = .boldSystemFont(ofSize: 16)
+    }
+}
